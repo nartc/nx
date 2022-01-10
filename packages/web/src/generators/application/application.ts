7f9221da9da86d5d2f53905025d5d1845321dce8
Chau Tran <nartc7789@gmail.com>
2022-01-10 09:33:48 -0600

    chore(testing): reenable swc/jest

--- packages/web/src/generators/application/application.ts
+++ packages/web/src/generators/application/application.ts
@@ -221,7 +221,7 @@ export async function applicationGenerator(host: Tree, schema: Schema) {
       project: options.projectName,
       skipSerializers: true,
       setupFile: 'web-components',
-      compiler: options.compiler as 'tsc',
+      compiler: options.compiler,
     });
     tasks.push(jestTask);
   }
